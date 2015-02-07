c855a010ad33cf0a75739312d59ce96a4fcf734d
david.norberg92@gmail.com <david.norberg92@gmail.com@76f9857c-9cb9-e7f6-f640-92a705d71585>
2015-02-07 13:56:04 +0000

    Player controller PHysics materials

--- trunk/Assets/Common/Scripts/PlayerController.cs
+++ trunk/Assets/Common/Scripts/PlayerController.cs
@@ -4,21 +4,37 @@ using System.Collections;
 public class PlayerController : MonoBehaviour 
 {
     [SerializeField]
-    private float m_speed;
+    private float m_speed = 50.0f;
+
+    [SerializeField]
+    private float m_jumpForce = 100.0f;
+
+    [SerializeField]
+    private Transform m_groundCheck = null;
+    [SerializeField]
+    private LayerMask m_groundLayers;
+
+    private bool m_grounded = false;
 	
-	void Start () 
+	void Start ()
     {
-	
+
 	}
 	
-	void FixedUpdate () 
+	void FixedUpdate ()
     {
+        m_grounded = Physics2D.OverlapCircle(m_groundCheck.position, 0.2f, m_groundLayers);
 
-        rigidbody2D.AddForce(new Vector2(Input.GetAxis("Horizontal") * m_speed, 0.0f) * Time.deltaTime);
+        float move = Input.GetAxis("Horizontal");
+        //rigidbody2D.AddForce(new Vector2(move * m_speed, 0.0f) * Time.deltaTime, ForceMode2D.Force);
+        rigidbody2D.velocity = new Vector2(move * m_speed, rigidbody2D.velocity.y);
+	}
 
-        if(Input.GetKeyDown(KeyCode.Space))
+    void Update()
+    {
+        if (m_grounded && Input.GetButtonDown("Jump"))
         {
-            rigidbody2D.AddForce(new Vector2(0.0f, 5.0f), ForceMode2D.Impulse);
+            rigidbody2D.AddForce(new Vector2(0.0f, m_jumpForce));
         }
-	}
+    }
 }
