a3f045a5f39f0191d1fde5a2d16ef9ea9e105bcd
david.norberg92@gmail.com <david.norberg92@gmail.com@76f9857c-9cb9-e7f6-f640-92a705d71585>
2015-02-07 20:01:25 +0000

    lots of stuff door animations

---
+++ trunk/Assets/Common/Scripts/MessagePopup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MessagePopup : MonoBehaviour 
+{
+    [SerializeField]
+    private string m_message;
+
+    private Text m_text;
+
+    void Awake()
+    {
+        m_text = GameObject.FindGameObjectWithTag("MessagePopup").GetComponent<Text>();
+    }
+
+    private void OnTriggerEnter2D()
+    {
+        m_text.text = m_message;
+    }
+
+    private void OnTriggerExit2D()
+    {
+        m_text.text = "";
+    }
+}
