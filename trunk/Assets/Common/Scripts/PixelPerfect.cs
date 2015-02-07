6729aa2dd377e3d1eb816cd1c68e6bfae537b13a
1992hg@gmail.com <1992hg@gmail.com@76f9857c-9cb9-e7f6-f640-92a705d71585>
2015-02-07 13:28:29 +0000

    added folders, with audio and fonts. Also added textures for amiga intro.

---
+++ trunk/Assets/Common/Scripts/PixelPerfect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PixelPerfect : MonoBehaviour
+{
+	public float Width;
+	public float Height;
+	public float PixelRatio;
+	void Update ()
+	{
+		Camera.main.orthographicSize = Width/(((Width/Height)*2)*PixelRatio);
+	}
+}
