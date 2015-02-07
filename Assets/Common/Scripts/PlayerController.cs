25a26aebdccfde77349e1417ea8b8f4499c7c471
david.norberg92 <david.norberg92@gmail.com>
2015-02-07 12:37:11 +0000

    folders and stuff

---
+++ Assets/Common/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+    [SerializeField]
+    private float m_speed;
+	
+	void Start () 
+    {
+	
+	}
+	
+	void FixedUpdate () 
+    {
+
+        rigidbody2D.AddForce(new Vector2(Input.GetAxis("Horizontal") * m_speed, 0.0f) * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody2D.AddForce(new Vector2(0.0f, 5.0f), ForceMode2D.Impulse);
+        }
+	}
+}
